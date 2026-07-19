93e999efaaee2d0c4ea86c9f2a0340033cce768f
Hayvita <hayvitark@gmail.com>
2026-07-19 20:00:45 +0530

    chore: add package.json with Tailwind CSS and PostCSS dependencies

---
+++ frontend/notes_app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Home from "./assets/pages/Home/Home";
+import Login from "./assets/pages/Login/Login";
+import SignUp from "./assets/pages/SignUp/SignUp";
+
+const App= () => {
+  return (
+    <div>
+    <Home />
+    <Login />
+    <SignUp />
+    </div>
+  )
+}
+
+export default App;
